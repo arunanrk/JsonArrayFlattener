995efb7b2ad2d4f4346c77169a83934e21ce48e5
arunanrk <35997127+arunanrk@users.noreply.github.com>
2018-06-21 18:23:36 +0530

    Update TestApp.java

--- src/main/java/org.github.json.flattener/TestApp.java
+++ src/main/java/org.github.json.flattener/TestApp.java
@@ -9,14 +9,14 @@ import java.util.List;
 public class TestApp {
     public static void main(String[] arg) throws Exception {
         TestApp app = new TestApp();
-        String content = app.readJsonFile("D:\\Projects\\b2m\\3b774348-9579-48ae-8798-d47fe225f684.json");
+        String content = app.readJsonFile("D:\\Projects\\json\\3b774348-9579-48ae-8798-d47fe225f684.json");
         List<String> jsonList = JsonArrayFlattener.getFlattenedJson(content);
         app.print(jsonList);
     }
 
     private void print(List<String> list) throws IOException, Exception {
         int i = 0;
-        FileWriter writer = new FileWriter("D:\\Projects\\b2m\\out.json");
+        FileWriter writer = new FileWriter("D:\\Projects\\json\\out.json");
         for(String value: list) {
             writer.write(value+"\n");
         }
